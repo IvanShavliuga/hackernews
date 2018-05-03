d3e551f959f36f3b950e8bbbe75ca95eca08bae0
Segun <segunolalive@gmail.com>
2018-05-03 09:34:31 +0100

    add navigation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-  <h2>{{ message }}</h2>
+    <navigation></navigation>
+    <main>
+      <router-view />
+    </main>
   </div>
 </template>
 
 <script>
+import Navigation from './components/Navigation/Navigation';
+import TopNews from './components/Pages/TopNews';
+
 export default {
   name: 'app',
   data () {
     return {
       message: 'Another Hacker News App'
     }
-  }
+  },
+  components: { Navigation },
 }
 </script>
 
+
+
 <style lang="scss">
+$nav-height: 4rem;
+html, body {
+  margin: 0;
+  box-sizing: border-box;
+  display: flex;
+  min-height: 100%;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: grid;
+  min-height: 100%;
+  width: 100vw;
+}
+
+main {
+  margin: 3.5rem auto;
 }
 
 h1, h2 {
---
+++ src/components/Navigation/Navigation.vue
@@ -0,0 +1,81 @@
+<template>
+  <nav>
+    <ul class="navigation" role="navigation">
+      <router-link
+        :to="urls[index]"
+        :key="link"
+        v-for="(link, index) in links"
+        tag="li"
+        active-class="active"
+        exact
+      >
+        <a>{{ link | capitalize }}</a>
+      </router-link>
+    </ul>
+  </nav>
+</template>
+
+
+<script>
+export default {
+  name: 'navigation',
+  data () {
+    return {
+      links: ['top', 'new', 'show', 'ask', 'jobs']
+    }
+  },
+
+  filters: {
+    capitalize(word) {
+      return `${word[0].toUpperCase()}${word.slice(1)}`
+    }
+
+  },
+
+  computed: {
+    urls() {
+      return this.links.map(link => link.split(' ').join('-'))
+    }
+  },
+
+  methods: {
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+nav {
+  background: black;
+  display: flex;
+  position: fixed;
+  height: 4rem;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 1000;
+}
+
+.navigation {
+  display: flex;
+  align-content: center;
+  align-items: center;
+  height: 3rem;
+  margin: 0px 20px;
+
+  ul {
+    height: 100%;
+  }
+
+  a {
+    text-decoration: none;
+    padding: 20px 5px;
+
+    &:hover {
+      color: #ffffff;
+      background-color: green;
+    }
+  }
+}
+</style>
+
