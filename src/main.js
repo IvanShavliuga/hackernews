ecd039353032877b0838b5f2ef4c0c2101c70211
Segun <segunolalive@gmail.com>
2018-05-03 09:36:18 +0100

    add router and vuex to entry point

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+export const eventBus = new Vue();
+
+
+Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router,
+  store,
 })
